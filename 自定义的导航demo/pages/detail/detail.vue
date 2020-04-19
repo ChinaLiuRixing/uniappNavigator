91e2e7f6ba671a4cad51778d7b7cbb444ddb06aa
chinaliurixing <chinaliurixing@163.com>
2020-04-19 16:31:22 +0800

    first commit

---
+++ 自定义的导航demo/pages/detail/detail.vue
@@ -0,0 +1,35 @@
+<template>
+	<view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onNavigationBarButtonTap(e) {
+			console.log("success",e);
+			if(e.id=='test1'){
+				uni.showToast({
+					title:"你点击了自定义按钮"
+				})
+			}else{
+				uni.showToast({
+					title:"你点击了飞机按钮"
+				})
+			}
+			
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ 自定义的导航demo/pages/index/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<view class="content">
+		<navigator url="../detail/detail">toDetail</navigator>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+</style>
